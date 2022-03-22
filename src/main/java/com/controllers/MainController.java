e5b94095e362bf933375ebd9e3310d1c084bf2f9
Hanna_Akhrameika <aneanra@yandex.ru>
2022-03-22 19:41:00 +0300

    final commit

--- src/main/java/com/controllers/MainController.java
+++ src/main/java/com/controllers/MainController.java
@@ -1,29 +1,51 @@
 package com.controllers;
 
-import com.service.Calculator;
-import org.springframework.cache.Cache;
+import com.entity.ExpressionRoot;
+import com.exeption.RootIsOutIntervalException;
+import com.service.CalculatorService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
 
 @Controller
 public class MainController {
 
-    @GetMapping("/kpp_labs")
+    @Autowired
+    private CalculatorService calculatorService;
+
+    @GetMapping("/")
     public String kpp_labs() {
-       return "forms";
+        return "forms";
     }
 
-    @PostMapping("/kpp_labs")
-    public String kpp_labs(@RequestParam float val1,
-                           @RequestParam float val2,
-                           @RequestParam float start,
-                           @RequestParam float end,
-                           Model model) {
-        model.addAttribute("root", Calculator.solve(val1, val2, start, end));
+    @GetMapping("/kpp_labs_html")
+    public String kpp_labs(@RequestParam(required = false) int val1, @RequestParam(required = false) int val2, @RequestParam(required = false) int start, @RequestParam(required = false) int end, Model model) {
+        model.addAllAttributes(new HashMap<String, Integer>() {
+            {
+                put("val1", val1);
+                put("val2", val2);
+                put("start", start);
+                put("end", end);
+            }
+        });
+        try {
+            ExpressionRoot root = calculatorService.solve(val1, val2, start, end);
+            model.addAttribute("root", "Корень " + root.getValue());
+        } catch (RootIsOutIntervalException ex) {
+            model.addAttribute("root", ex.getMessage());
+        }
         return "forms";
+    }
+
+    @GetMapping(value = "/kpp_labs")
+    @ResponseBody
+    public ExpressionRoot mainJSONObject(@RequestParam int val1, @RequestParam int val2, @RequestParam int start, @RequestParam int end) throws RootIsOutIntervalException {
+        return calculatorService.solve(val1, val2, start, end);
     }
 
 }
---
+++ src/main/java/com/entity/ExpressionRoot.java
@@ -0,0 +1,18 @@
+package com.entity;
+
+public class ExpressionRoot {
+
+	private int value;
+
+	public ExpressionRoot(int value) {
+		this.value = value;
+	}
+
+	public int getValue() {
+		return value;
+	}
+
+	public void setValue(int value) {
+		this.value = value;
+	}
+}
