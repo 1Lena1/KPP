84e2f292cc257cce813f64228534cf3d8b1980e8
Hanna_Akhrameika <aneanra@yandex.ru>
2022-03-22 17:49:01 +0300

    delete files

--- src/main/java/com/service/Calculator.java
+++
@@ -1,9 +0,0 @@
-package com.service;
-
-public class Calculator {
-    public static String solve(float val1, float val2, float start, float end) {
-        if (val2 - val1 >= start && val2 - val1 <= end)
-            return Float.toString(val2 - val1);
-        return "";
-    }
-}
